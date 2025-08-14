bb20a39fc72dbfc61eda0e973c647b470acb8994
Homura <egzozu.be1.bas@gmail.com>
2025-08-14 09:23:20 +0300

    forgor

--- source/funkin/options/categories/MobileOptions.hx
+++ source/funkin/options/categories/MobileOptions.hx
@@ -4,6 +4,7 @@ class MobileOptions extends TreeMenuScreen {
 	public function new() {
 		super('Mobile', 'Change Mobile Related Things such as Controls alpha, screen timeout....', 'MobileOptions.');
 
+		#if TOUCH_CONTROLS
 		add(new ArrayOption("Extra Hints", "Select how many extra hints you prefer to have on hitbox", ['NONE', 'SINGLE', 'DOUBLE'], ["None", "Single", "Double"], 'extraHints'));
 		add(new NumOption("Hitbox Opacity", "Change how opaque the Hitbox should be", 0.0, 1.0, 0.1, "hitboxAlpha"));
 		add(new Checkbox("Use Old Pad Texture", "If checked, the TouchPad will use the old texture it used before", "oldPadTexture"));
