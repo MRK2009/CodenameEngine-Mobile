7b69b3cb600d712e3143975e6512d960a5eac8ef
Karim Akra <144803230+KarimAkra@users.noreply.github.com>
2025-08-14 22:24:23 +0300

    TurboControls don

--- source/funkin/backend/TurboControls.hx
+++ source/funkin/backend/TurboControls.hx
@@ -55,10 +55,10 @@ class TurboControls extends TurboBasic {
 
 	override function get_pressed() {
 		if (allPress) {
-			for (control in controls) if (!controlsInstance.getActionFromControl(control).check()) return false;
+			for (control in controls) if (!(controlsInstance.getActionFromControl(control).check() || controlsInstance.mobileControlsPressed(getMobileIDFromControl(control)))) return false;
 		}
 		else {
-			for (control in controls) if (controlsInstance.getActionFromControl(control).check()) return true;
+			for (control in controls) if (controlsInstance.getActionFromControl(control).check() || controlsInstance.mobileControlsPressed(getMobileIDFromControl(control))) return true;
 		}
 		return allPress;
 	}
